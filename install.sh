108ee124ecd87e7bc1d3d66c7ec9c07d527b359f
Sebastian Krüger <sk@honeymachine.io>
2024-11-08 00:03:26 +0100

    feat: build image

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ python3 -m venv $PTYHON_VENV_HOME
 echo -e "${BIGreen}venv available under $PTYHON_VENV_HOME${Color_Off}"
 
 echo -e "${BIPurple}Installing requirements in $H1VE_HOME${Color_Off}"
-$PIP_BIN install --quiet --no-python-version-warning -r $H1VE_HOME/requirements.txt
+$PIP_BIN install --quiet --use-pep517 -r $H1VE_HOME/requirements.txt
 echo -e "${BIGreen}Ansible available via $ANSIBLE_BIN${Color_Off}"
 
 echo -e "${BIBlue}Running playbook $ANSIBLE_PLAYBOOK${Color_Off}"
