968450f576b99e777d8787c4c034676b0ea0c3b3
Sebastian Krüger <sk@honeymachine.io>
2024-11-09 18:10:10 +0100

    fix: hostname

--- src/core/app/app.py
+++ src/core/app/app.py
@@ -3,7 +3,7 @@ import redis
 
 app = Flask(__name__)
 
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client = redis.StrictRedis(host='h1ve-core-redis', port=6379, db=0)
 channel = 'h1ve'
 
 @app.route("/")
